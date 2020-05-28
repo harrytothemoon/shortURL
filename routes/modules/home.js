12609689e2632e422ba0f3c8a32e784e332eaa3c
Harry <harry811016@gmail.com>
2020-05-29 00:36:50 +0800

    modify logic of repeat random number and change to async/await

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -30,44 +30,32 @@ router.get('/new', (req, res) => {
 router.post('/new', async (req, res, next) => {
   const urloriginal = req.body.url
   const checkUrl = await urlExist(urloriginal)
+  //1.先確認輸入的網址是否有效，有效則進行下一步，無效則跳出提醒
+  //2.再確認資料庫是否有值，有的話則提醒已經有短網址可用，無的話則進行下一步判斷
+  //3.最後判斷是否產生的5碼亂數是否有重複，有的話則進入迴圈，無的話則產生短網址
   if (!checkUrl) return res.send(`
         <h3>Please input a valid URL！</h3>
         <button type="button" onclick="history.back()">Back</button>
         `);
-  urldata.findOne({ urlFull: urloriginal }).lean()
-    .then((url) => {
-      //1.先確認輸入的網址是否有效，有效則進行下一步，無效則跳出提醒
-      //2.再確認資料庫是否有值，有的話則提醒已經有短網址可用，無的話則進行下一步判斷
-      //3.最後判斷是否產生的5碼亂數是否有重複，有的話則進入迴圈，無的話則產生短網址
-      if (url) {
-        const existMessage = 'The URL you entered has been shorten previously,'
-        return res.render('new', { url, existMessage })
-      }
-      if (url === null) {
-        let random = `https://secret-thicket-82895.herokuapp.com/${generateGarbled(5)}`
-        urldata.find()
-          .then(checkShort => {
-            const checkExist = checkShort.map(x => x.urlShort)
-            while (checkExist.includes(random)) {
-              random = `https://secret-thicket-82895.herokuapp.com/${generateGarbled(5)}`
-            }
-            return random
-          })
-          .then(() => {
-            const newUrl = new urldata({ urlFull: urloriginal })
-            newUrl.urlShort = random
-            return newUrl.save()
-              .then(() => {
-                urldata.findOne({ urlFull: urloriginal })
-                  .lean()
-                  .then((url) => {
-                    res.render('new', { url })
-                  })
-              })
-          })
-      }
-    })
-    .catch(error => console.log(error))
+  try {
+    const url = await urldata.findOne({ urlFull: urloriginal }).lean()
+    if (url) {
+      const existMessage = 'The URL you entered has been shorten previously,'
+      return res.render('new', { url, existMessage })
+    }
+    let random = `https://secret-thicket-82895.herokuapp.com/${generateGarbled(5)}`
+    const checkShort = await urldata.find().lean()
+    while (checkShort.map(x => x.urlShort).includes(random)) {
+      random = `https://secret-thicket-82895.herokuapp.com/${generateGarbled(5)}`
+    }
+    const newUrl = new urldata({ urlFull: urloriginal })
+    newUrl.urlShort = random
+    await newUrl.save()
+    res.render('new', { url: newUrl.toObject() })
+  } catch (e) {
+    console.log(e)
+    res.sendStatus(404)
+  }
 })
 
 // Delete Function
